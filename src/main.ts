c68310f2116e4dbc5fffc9c12db1afda9f5f3a4a
Botan Coban <botancoban79@gmail.com>
2025-06-10 13:30:54 +0200

    feat: CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,23 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.enableCors({
+    origin: true, // Erlaubt alle Origins, wird geändert
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Whisper World API')
     .setDescription('API for the Whisper World project')
     .setVersion('1.0')
     .build();
-  
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
